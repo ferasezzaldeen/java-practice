3cfe489b0f2f5dc7dd9e7bbad73b252d8add02ac
ferasezzaldeen <ferasezaldeen@gmail.com>
2022-06-25 21:54:10 +0300

    sec commit

--- src/firstPackage/Main.java
+++ src/firstPackage/Main.java
@@ -2,6 +2,7 @@ package firstPackage;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("gigity");
         System.out.println("hello there");
     }
 }
